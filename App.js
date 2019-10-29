47750e59d261aa28bd704c87f7852aeed46c3d15
Iris <osbornem406@gmail.com>
2019-10-29 16:22:13 +0000

    Logged Out Screen done

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import LoggedOut from "./containers/screens/LoggedOut";
+
+export default function App() {
+  return <LoggedOut />;
+}
